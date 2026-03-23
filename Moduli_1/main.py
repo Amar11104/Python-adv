5ec528b970b33b3e169b157decea3d2d2c43151d
Amar11104 <amar.dushulovci@gmail.com>
2026-03-23 20:42:03 +0100

    moduli 2

--- Moduli_1/main.py
+++ Moduli_1/main.py
@@ -27,3 +27,19 @@ name_of_the_list = ["item 1","item 2","item 3","item 4"]
 shoping_list = ["apples","Bananas", 3, 4.5]
 
 print(shoping_list[1])
+
+shopping_list.append("oranges")
+print(shopping_list)
+
+shopping_list.insert(2,"lemons")
+print(shopping_list)
+
+shoping_list.remove("oranges")
+print(shopping_list)
+
+del_shopping_list[2]
+print(shopping_list)
+
+shopping_list[0] = "cherry"
+print(shopping_list)
+
