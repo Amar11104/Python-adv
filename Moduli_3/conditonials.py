55718891aad674db88f54b4ddc5f47b2886ceb56
Amar11104 <amar.dushulovci@gmail.com>
2026-03-25 20:33:58 +0100

    Moduli 3

---
+++ Moduli_3/conditonials.py
@@ -0,0 +1,25 @@
+from sklearn.externals.array_api_compat.cupy.fft import ifftshift
+
+age = 18
+
+if age >=18:
+    print("You can vote.")
+else:
+    print("you cant vote")
+temp = 28
+if temp > 30:
+    print("Its a hot day")
+elif 20<= temp <=30:
+    print(The weather is pleasing)
+
+else:
+    print("Its a cold day")
+
+number = 7
+
+if number % 2 ==0:
+    print("Numri eshte qift")
+
+else:
+    print("Numri eshte tek")
+
